40b5534da369b2c50783a5493a0142fd410259b3
Kenichi.Nagahara <kenichi.nagahara@daikin.co.jp>
2019-01-26 17:47:28 +0900

    1/26

---
+++ HTML/folderPass.bat
@@ -0,0 +1,6 @@
+Dir /b C:\D\desk\github\General\HTML\root >C:\D\desk\github\General\HTML\root\root.txt
+
+Dir /b C:\D\desk\github\General\HTML\root\temp1 >C:\D\desk\github\General\HTML\root\temp1.txt
+
+Dir /b C:\D\desk\github\General\HTML\root\temp2 >C:\D\desk\github\General\HTML\root\temp2.txt
+
